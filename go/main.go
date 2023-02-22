fb8276b1b00ea3289c5cc9b5c53f4542b08c0e41
manewilk <raducristianstefan2002@gmail.com>
2023-02-22 19:54:09 +0200

    start with go

---
+++ go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var c1 int = 3
+	var c2 = 2
+	c3 := 1
+
+	const myc1 = 44
+
+	fmt.Println("Hello, cristelu gopherul")
+	fmt.Println(c1)
+	fmt.Println(c2)
+	fmt.Println(c3)
+	fmt.Println(myc1)
+
+}
